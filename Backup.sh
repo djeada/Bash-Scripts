9b31856fb4cd939ff01e075bf066ffe686e1cdd4
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2019-03-25 22:17:22 +0100

    Add files via upload

--- Backup.sh
+++ Backup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-#what files or folders to tak backup of.
-sourceFolder="/etc"
+#what files or folders to tak backup of
+declare -a arr=("/home/adam/Desktop" "/etc" "/home/adam/Desktop/Rogue")
 
-#destination of backup.
+#sourceFolder="/home/adam/Desktop"
+
+#destination of backup
 destFolder="/home/adam/Desktop"
+
+#store all the backup folders in one directory
+mkdir backupDir
+
+for i in "${arr[@]}"
+do
+	sudo cp -Rf $i backupDir
+done
 
 #declaring max Size
 maxSize="10M"
@@ -12,7 +22,7 @@ maxSize="10M"
 #excluding file extansions
 exclusion="*.sh"
 
-#create archive filename.
+#create archive filename
 day=$(date +%Y-%m-%d)
 hostname=$(hostname -s)
 archive_file="$hostname-$day.tar.gz"
@@ -22,8 +32,9 @@ echo "Backing up $sourceFolder to $dest/$archive_file"
 date
 echo
 
-#Backup the files using tar.
-tar -zcvf $destFolder/$archive_file --exclude=$exclusion --exclude-from <(find $sourceFolder -size +$maxSize) $sourceFolder
+#Backup the files using tar
+tar -zcvf $destFolder/$archive_file --exclude=$exclusion --exclude-from <(find backupDir -size +$maxSize) backupDir
+
  
 #print end status message
 echo
@@ -36,7 +47,6 @@ ls -lh $dest
 #encryption
 gpg -c $destFolder/$archive_file
 
-#remove unencrypted file
+#remove unencrypted backup file
 rm -r $destFolder/$archive_file
-
 
