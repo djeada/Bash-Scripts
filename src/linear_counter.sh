a36b3d8afda7aec773d08a433d044a5e99c7b072
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-07-17 22:34:33 +0200

    Create linear_counter.sh

---
+++ src/linear_counter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
+fileName=$1
+
+if [ ! -f "$fileName" ]; then
+    echo "$fileName does not exist."
+    exit 1
+fi
+
+counter=0
+
+while read p; do
+  ((counter++))
+done < $fileName
+
+echo "Number of lines in ${fileName} is: ${counter}" 
