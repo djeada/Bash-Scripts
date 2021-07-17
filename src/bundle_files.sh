1104cb253080d3fa69dd25b275f9029dabeb0db1
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-07-17 23:56:27 +0200

    Create bundle_files.sh

---
+++ src/bundle_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in $@; do
+  if [ ! -f "$i" ]; then
+    echo "$i does not exist."
+    exit 1
+  fi
+  echo "unpacking file $i"
+  echo "cat > $i <<EOF"
+  cat $i
+  echo "EOF"
+done
