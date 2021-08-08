7dd71b6db315affd694a3bf73b13a069949f357b
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-08-08 23:20:48 +0200

    Update remove_diacritics.sh

--- src/remove_diacritics.sh
+++ src/remove_diacritics.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
-sed -i 'y/ąāáǎàćēéěèęīíǐìńōóǒòóśūúǔùǖǘǚǜżźĄĀÁǍÀĆĒĘÉĚÈĪÍǏÌŃŌÓǑÒÓŚŪÚǓÙǕǗǙǛŻŹ/aaaaaceeeeeiiiinooooosuuuuüüüüzzAAAAACEEEEEIIIINOOOOOSUUUUÜÜÜÜZZ/' $1
+
+remove_diactrics ()
+{
+    sed -i 'y/ąāáǎàćēéěèęīíǐìńōóǒòóśūúǔùǖǘǚǜżźĄĀÁǍÀĆĒĘÉĚÈĪÍǏÌŃŌÓǑÒÓŚŪÚǓÙǕǗǙǛŻŹ/aaaaaceeeeeiiiinooooosuuuuüüüüzzAAAAACEEEEEIIIINOOOOOSUUUUÜÜÜÜZZ/' $1
+}
+
+main() {
+
+    if [ $# -eq 0 ]; then
+        echo "Must provide the path!"
+        exit 1
+    fi
+
+    if [ $1 == '.' ] || [ -d "${1}" ]; then
+        for file in $(find $1 -maxdepth 10 -type f)
+        do
+            remove_diactrics $file
+        done
+    elif [ -f "${1}" ]; then
+        remove_diactrics $1
+    else
+        echo "$1 is not a valid path!"
+    fi
+
+}
+
+main "$@"
