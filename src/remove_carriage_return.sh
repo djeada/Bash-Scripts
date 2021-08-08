9f7d767856edf9dfc27a3c65e6e23e9027f3943c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-08-08 23:46:42 +0200

    Update remove_carriage_return.sh

--- src/remove_carriage_return.sh
+++ src/remove_carriage_return.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
-sed -i 's/\r//g' $1
+
+remove_carriage_return ()
+{
+    sed -i 's/\r//g' $1
+}
+
+main() {
+
+    if [ $# -eq 0 ]; then
+        echo "Must provide the path!"
+        exit 1
+    fi
+
+    if [ $1 == '.' ] || [ -d "${1}" ]; then
+        for file in $(find $1 -maxdepth 10 -type f)
+        do
+            remove_carriage_return $file
+        done
+    elif [ -f "${1}" ]; then
+        remove_carriage_return $1
+    else
+        echo "$1 is not a valid path!"
+    fi
+
+}
+
+main "$@"
