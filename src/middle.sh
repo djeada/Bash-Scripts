48c998d09836d51cdfc84e7798232da79cd359cb
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-08-15 00:57:56 +0200

    Update middle.sh

--- src/middle.sh
+++ src/middle.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/env bash
-head -22 | tail -11
+
+main() {
+    if [ $# -eq 0 ]; then
+        echo "Must provide the path!"
+        exit 1
+    fi
+
+    if [ ! -f $1 ]; then
+        echo "$1 is not a valid path!"
+        exit 1
+    fi
+
+    middle=$(($(sed -n '$=' $1)/2))
+    head -$middle $1 | tail -n +$middle
+}
+
+main "$@"
