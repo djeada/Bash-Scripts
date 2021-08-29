07116bc8a5948b040bfde2c611f50176a97316b7
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-08-29 10:52:35 +0200

    Update factorial.sh

--- src/factorial.sh
+++ src/factorial.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+if [ $# -eq 0 ]; then
+    echo "Must provide exactly one number!"
+    exit 1
+fi
+
+re='^[0-9]+$'
+if ! [[ $1 =~ $re ]]; then
+   echo "$1 is not a positive integer!"
+   exit 1
+fi
+
 fact=1
 num=$1
+
 while [ $num -gt 1 ]
 do
     fact=$((fact * num))  #fact = fact * num
