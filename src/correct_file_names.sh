33236e24b7935ed76e25688675af5fc8f097e14a
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-09-07 15:38:52 +0200

    Update correct_file_names.sh

--- src/correct_file_names.sh
+++ src/correct_file_names.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
+correct_file_name ()
+{
+    new_name=`echo $1 | sed -e 's/ /_/g' | tr '[:upper:]' '[:lower:]'`
+    mv "$1" "$new_name"
+}
+
 main() {
 
-    for old_name in $@; do
-        if [ ! -f "$i" ]; then
-            echo "$i does not exist."
-            exit 1
-        fi
-        
-        new_name=`echo $old_name | sed -e 's/ /_/g'` | tr '[:upper:]' '[:lower:]'` 
-        mv "$old_name" "$new_name" 
-    done
+    if [ $# -eq 0 ]; then
+        echo "Must provide the path!"
+        exit 1
+    fi
+
+    if [ $1 == '.' ] || [ -d "${1}" ]; then
+        for file in $(find $1 -maxdepth 10 -type f)
+        do
+            correct_file_name $file
+        done
+    elif [ -f "${1}" ]; then
+        correct_file_name $1
+    else
+        echo "$1 is not a valid path!"
+    fi
 
 }
 
