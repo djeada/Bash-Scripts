72a8c1eb25e590ffedfb25729426a178aaf9512e
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-09-10 09:47:10 +0200

    Create check_if_root.sh

---
+++ src/check_if_root.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+main() {
+
+  if [ $(id -u) -ne 0 ]; then
+    echo "This script must be run with the privileges of root. Try: sudo bash $0"
+    exit 1
+  fi
+
+}
+
+main "$@"
