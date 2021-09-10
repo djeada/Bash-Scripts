76a3139456df759cd8ec5a5e774562fd4f22d03d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-09-10 09:42:49 +0200

    Create ram_memory.sh

---
+++ src/ram_memory.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#In kB
+MINIMUM=100000000
+MIN_READABLE=$(echo "scale=2; $a/1024/1024" | bc -l)
+
+main() {
+
+    ram="$(free | awk '/^Mem:/{print $2}')"
+    if [ $ram -lt $MINIMUM ]; then
+      echo "The system doesn't meet the requirements. Memory must be at least $MIN_READABLE GB."
+      exit 1
+    fi
+
+}
+
+main "$@"
