64ae7b82596f4bb0a5a41e6cf64fe1e119c4b444
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-09-16 10:24:48 +0200

    checking if provided branch exists before executing the commands

--- src/remove_branch.sh
+++ src/remove_branch.sh
@@ -2,9 +2,31 @@
 
 main() {
 
-    branch_name=$1
-    git push -d origin $branch_name
-    git branch -D $branch_name
+    if [ $# -eq 0 ]; then
+        echo "you have to provide the branch name!"
+        exit 1
+    elif [ $# -eq 1 ]; then
+        branch_name="$1"
+    else
+        echo "currently not supported!"
+        exit 1
+    fi
+
+    is_remote_branch=$(git branch -r | grep -w $branch_name > /dev/null)
+    is_local_branch=$(git branch -l | grep -w $branch_name > /dev/null)
+
+    if [ $is_remote_branch -ne 0 ] && [ $is_local_branch -ne 0 ]; then
+        echo "provided branch doesn't exists"
+        exit 1
+    fi
+
+    if [ $is_local_branch -eq 0 ]; then
+        git push -d origin $branch_name
+    fi
+
+    if [ $is_remote_branch -eq 0 ]; then
+        git branch -D $branch_name
+    fi
 
 }
 
