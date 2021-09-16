f02333399bfb20a6eb51acc2acd5aa032f487bb8
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-09-16 10:01:21 +0200

    adding option to specify the path to a repo

--- src/count_lines_of_code.sh
+++ src/count_lines_of_code.sh
@@ -2,8 +2,17 @@
 
 main() {
 
-   git ls-files -z | xargs -0 wc -l | awk 'END{print}'
+    if [ $# -eq 0 ]; then
+        path="."
+    elif [ -d $1 ]; then
+        path="$1"
+    else
+        echo "provided path is not valid!"
+        exit 1
+    fi
 
+    cd $path
+    git ls-files -z | xargs -0 wc -l | awk 'END{print}'
 }
 
 main "$@"
