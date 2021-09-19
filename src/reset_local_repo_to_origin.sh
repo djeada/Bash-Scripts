c34cd43148a6085aa233b4753f9f0fa087dcd2a1
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-09-20 00:27:13 +0200

    adding option to specify working dir and branch name

--- src/reset_local_repo_to_origin.sh
+++ src/reset_local_repo_to_origin.sh
@@ -1,12 +1,43 @@
 #!/usr/bin/env bash
 
-# TODO: working_dir and branch names as parameter
-
 main() {
 
-  git fetch origin
-  git reset --hard origin/master
-  git clean -fdx
+    working_dir="."
+
+    if [ $# -eq 0 ]; then
+        echo "You have to specify the branch name!"
+        exit 1
+    fi
+
+    if [ $# -eq 1 ] || [ $# -eq 2 ]; then
+        branch_name="$1"
+        is_remote_branch=$(git branch -r | grep -Fw $branch_name > /dev/null)
+        is_local_branch=$(git branch -l | grep -Fw $branch_name > /dev/null)
+
+        if [ $is_remote_branch -ne 0 ] && [ $is_local_branch -ne 0 ]; then
+            echo "provided branch doesn't exists"
+            exit 1
+        fi
+
+        if  [ $# -eq 2 ]; then
+            working_dir="$2"
+            if [ ! -d "$working_dir" ]; then
+                echo "$workind_dir is not a directory."
+                exit 1
+            fi
+        fi
+
+    else
+        echo "You can't specify more than 2 parameters!"
+        exit 1
+    fi
+
+    cd "$working_dir"
+
+    git fetch origin
+    git checkout "$branch_name"
+    git reset --hard origin/"$branch_name"
+    git clean -fdx
 
 }
 
