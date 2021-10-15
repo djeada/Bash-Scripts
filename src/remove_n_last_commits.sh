571ac987f1344c6e9e68dbc26ffde9095032dcbe
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-10-15 23:18:21 +0200

    Delete one level of nested conditions from the script

--- src/remove_n_last_commits.sh
+++ src/remove_n_last_commits.sh
@@ -10,37 +10,37 @@ main() {
         exit 1
     fi
 
-    if [ $# -ge 1 ] && [ $# -le 3 ]; then
-        n="$1"
-
-        if  [ $# -eq 2 ]; then
-            working_dir="$3"
-            if [ ! -d "$working_dir" ]; then
-                echo "$workind_dir is not a directory."
-                exit 1
-            fi
-            cd "$working_dir"
-        fi
-        
-        is_git_repo=$(git rev-parse --git-dir > /dev/null 2>&1)
-        
-        if [ ${#is_git_repo} -gt 0 ]; then
-          echo "Not inside a git repo! Please provide a correct path."
-          exit 1
-        fi
-
-        if  [ $# -eq 3 ]; then
-            branch_name="$2"
-            is_local_branch=$(git branch -l | grep -Fw $branch_name)
-            
-            if [ ${#is_local_branch} -eq 0 ]; then
-                echo "provided branch doesn't exists"
-                exit 1
-            fi
-        fi
-    else
+    if [ $# -gt 3 ]; then
         echo "You can't specify more than 3 parameters!"
         exit 1
+    fi
+    
+    n="$1"
+
+    if  [ $# -eq 2 ]; then
+        working_dir="$3"
+        if [ ! -d "$working_dir" ]; then
+            echo "$workind_dir is not a directory."
+            exit 1
+        fi
+        cd "$working_dir"
+    fi
+
+    is_git_repo=$(git rev-parse --git-dir > /dev/null 2>&1)
+
+    if [ ${#is_git_repo} -gt 0 ]; then
+      echo "Not inside a git repo! Please provide a correct path."
+      exit 1
+    fi
+
+    if  [ $# -eq 3 ]; then
+        branch_name="$2"
+        is_local_branch=$(git branch -l | grep -Fw $branch_name)
+
+        if [ ${#is_local_branch} -eq 0 ]; then
+            echo "provided branch doesn't exists"
+            exit 1
+        fi
     fi
 
     git checkout "$branch_name"
