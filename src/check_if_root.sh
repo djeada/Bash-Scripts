4561acaf2c4135e0775c34341aa9aaeccad0827c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-10-18 22:00:03 +0200

    fixing a typo

--- src/check_if_root.sh
+++ src/check_if_root.sh
@@ -3,7 +3,7 @@
 main() {
 
     if [ $(id -u) -ne 0 ]; then
-        echo "This script must be run with the privileges of root. Try: sudo bash $0"
+        echo "This script must be executed with root privileges. Try: sudo bash $0"
         exit 1
     fi
 
