79ae67829e23e43954dd1884ef14218a4dabeaaa
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-10-22 16:55:19 +0200

    Correcting typos and formatting

--- src/make_dir.sh
+++ src/make_dir.sh
@@ -2,16 +2,16 @@
 
 main() {
 
-    echo "Enter a directory name:"
+    echo "Enter the directory name:"
 
     read dir_name
 
-    if [-d "$dir_name" ]
+    if [ -d "$dir_name" ]
     then
-        echo "Directory already exists!"
+        echo "There is already a directory with that name!"
     else
-        `mkdir $dir_name`
-        echo "Directory has been created!"
+        mkdir "$dir_name"
+        echo "A directory has been created!"
     fi
 
 }
