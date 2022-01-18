3f7a621240cdef1271430ac3fe2d1249e5b62a1c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-01-19 00:26:19 +0100

    Create program_on_port.sh

---
+++ src/program_on_port.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Script Name: program_on_port.sh
+# Description: Script to check which programs are running on a specific port. 
+# Usage: program_on_port.sh [port]
+#       [port] - a port to check
+# Example: ./program_on_port.sh 8001
+
+main() {
+    if [ $# -ne 1 ]; then
+        echo "Usage: program_on_port.sh [port]"
+        echo "       [port] - a port to check"
+        echo "Example: ./program_on_port.sh 8001"
+        exit 1
+    fi
+    
+   re='^[0-9]+$'
+      if ! [[ $1 =~ $re ]]; then
+          echo "Error: $1 is not an integer"
+          exit 1
+      fi
+
+  lsof -i tcp:"$1"
+
+}
+
+main "$@"
