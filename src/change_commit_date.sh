3fac2a57e2c815f64c742112b11f0dbe8f36595f
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-02-04 17:28:32 +0100

    Update change_commit_date.sh

--- src/change_commit_date.sh
+++ src/change_commit_date.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# TODO: accept only the date, use random time, and calculate the day of the week based on the date
+
 LC_ALL=C GIT_COMMITTER_DATE="$1 +0100" git commit --amend --no-edit --date "$1 +0100"
 
