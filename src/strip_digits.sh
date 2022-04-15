868cb9e173e97658b49adcf6de677aa97c47ce48
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-04-15 23:04:11 +0200

    Create strip_digits.sh

---
+++ src/strip_digits.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Script Name: strip_digits.sh
+# Description: Removes all digits from each string in a given file. 
+# Usage: strip_digits.sh [<file_path>]
+#        [<file_path>] - the path to the file to process.
+# Example: ./strip_digits.sh path/to/file
+
+main() {
+    if [ $# -eq 0 ]; then
+        echo "You must provide a file path!"
+        exit 1
+    fi
+
+    if [ ! -f "$1" ]; then
+        echo "$1 is not a valid file path!"
+        exit 1
+    fi
+
+    local file_path=$1
+    local file_name
+    file_name=$(basename "$file_path")
+
+    sed -r 's/[^[:space:]]*[0-9][^[:space:]]* ?//g' "$file_path" > "$file_name".tmp
+    mv "$file_name".tmp "$file_name"
+}
+
+main "$@"
