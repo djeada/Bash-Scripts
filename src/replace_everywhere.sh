1609e2d27e5ecdc6178c6070d600466c00688a1b
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-05-01 02:08:47 +0200

    Update replace_everywhere.sh

--- src/replace_everywhere.sh
+++ src/replace_everywhere.sh
@@ -6,6 +6,9 @@
 #       [string_a] - Old string.
 #       [string_b] - New string.
 # Example: replace_everywhere.sh "cat" "act"
+#
+# For strings with spaces use single quotes: replace_everywhere.sh 'string with space' ''
+# Escape special characters with single slash: replace_everywhere.sh 'string\*\*' ''
 
 main() {
 
@@ -13,8 +16,6 @@ main() {
         echo "Usage: hamming_distance.sh [string_a] [string_b]"
         return
     fi
-
-    echo "$1 $2"
 
     find . -type f -exec sed -i -r -e "s/$1/$2/g" {} \;
 }
