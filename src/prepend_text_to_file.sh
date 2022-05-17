1e4c07f36cc6f63562683057e7365b6d5825d0a0
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-05-17 22:00:28 +0200

    Create prepend_text_to_file.sh

---
+++ src/prepend_text_to_file.sh
@@ -0,0 +1,33 @@
+
+prepend_text_to_file() {
+    
+    # assure number of arguments is correct
+    if [ $# -ne 2 ]; then
+        echo "Usage: prepend_text_to_file <file> <text>"
+        return 1
+    fi
+
+    local file="$1"
+    local text="$2"
+
+    if [ -f "$file" ]; then
+        echo "$text" | cat - "$file" > temp && mv temp "$file"
+    else
+        echo "$text" > "$file"
+    fi
+
+    # Display log info in the console
+    echo "The following text was added to the file $file:"
+    echo "$text"
+
+}
+
+
+main() {
+
+    # call the function
+    prepend_text_to_file "$1" "$2"
+
+}
+
+main "$@"
