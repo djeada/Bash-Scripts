bede44c74232d08bb76c0c5f0352964789121ef9
djeada <addjellouli1@gmail.com>
2022-05-18 21:19:02 +0200

    [feature]: converting all videos from url

--- src/youtube_to_mp3.sh
+++ src/youtube_to_mp3.sh
@@ -37,17 +37,31 @@ try_to_install_dependencies() {
 download_video() {
     local url="$1"
     local output_file="$2"
+    echo "Attempting to download video from '$url'"
     youtube-dl -f bestaudio -o "$output_file" "$url"
+    echo "Saving video to '$output_file'"
 }
 
 
 convert_video_to_mp3() {
     local input_file="$1"
     local output_file="${input_file%.*}.mp3"
+    echo "Converting video to mp3"
     ffmpeg -i "$input_file" -vn -ab 128k "$output_file"
+    echo "Audio file saved to '$output_file'"
     rm "$input_file"
+    echo "Video file deleted"
 }
 
+is_url_is_playlist() {
+    local url="$1"
+    local playlist_id=$(youtube-dl -j --flat-playlist "$url" | jq -r '.id')
+    if [ "$playlist_id" != "null" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
 main() {
     if [ $# -lt 1 ]; then
@@ -71,6 +85,18 @@ main() {
         output_file=$(youtube-dl --get-title "$url")
         #convert spaces to underscores and make lowercase
         output_file=$(echo "$output_file" | tr ' ' '_' | tr '[:upper:]' '[:lower:]')
+    fi
+
+    if is_url_is_playlist "$url"; then
+        echo "Downloading playlist..."
+        local all_videos_urls=$(youtube-dl -j --flat-playlist "$url" | jq -r '.id' | sed 's_^_https://youtu.be/_')
+        for video_url in $all_videos_urls; do
+            download_video "$video_url" "$output_file"
+            convert_video_to_mp3 "$output_file"
+        done
+    else
+        download_video "$url" "$output_file"
+        convert_video_to_mp3 "$output_file"
     fi
 
     download_video "$url" "$output_file"
