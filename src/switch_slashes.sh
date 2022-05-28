a8331b3cf3410070bc60e6f7fa54d1a5a253e87b
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-05-28 22:24:10 +0200

    Update switch_slashes.sh

--- src/switch_slashes.sh
+++ src/switch_slashes.sh
@@ -2,18 +2,16 @@
 
 # Script Name: switch_slashes.sh
 # Description: Script to replace left slashes with right slashes and vice versa
-# Usage: switch_slashes.sh [file_name] [<direction>]
+# Usage: switch_slashes.sh [file_name] <direction>
 #       [file_name] - file to be changed
-#       [<direction>] - three options possible: a) left_to_right b) right_to_left c) invert
-#       if not specified the direction is set to invert
+#       <left_to_right | right_to_left | invert> - if not specified the direction is set to invert 
 # Example: ./switch_slashes.sh left_to_right
 
 main() {
     if [ $# -eq 0 ] || [ $# -gt 2 ]; then
         echo "Usage: switch_slashes.sh [file_name] [<direction>]"
         echo "       [file_name] - file to be changed"
-        echo "       [<direction>] - three options possible: a) left_to_right b) right_to_left c) invert"
-        echo "       if not specified the direction is set to invert"
+        echo "       <left_to_right | right_to_left | invert> - if not specified the direction is set to invert"
         echo "Example: ./switch_slashes.sh left_to_right"
         exit 1
     fi
