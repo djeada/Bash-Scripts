3bc072f3e0761c3f5f28f8d015beb6b3ff525617
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-09-21 10:05:18 +0200

    Create download_all_github_repos.sh

---
+++ src/download_all_github_repos.sh
@@ -0,0 +1,32 @@
+download_repos() {
+  user_name="$1"
+  temp_dir=$(mktemp -q /tmp/repo_archive.XXXXXX)
+  if [ $? -ne 0 ]; then
+      echo "$0: Can't create a temp file!"
+      exit 1
+  fi
+  
+  cd "$temp_dir"
+  virtualenv env
+  source env/bin/activate
+  pip install ghcloneall
+  ghcloneall --init --user "$user_name"
+  ghcloneall
+  deactivate
+  cd ~
+  destination="$1""_repo_archive.tar"
+  tar -cvf "$destination" "$temp_dir"
+  rm -rf "$temp_dir"
+  echo "All repositories of $user_name have been written to $destination"
+}
+
+main() {
+    if [[ $# -ne 1 ]]; then
+        echo "Usage: download_all_github_repos.sh [github_username]"
+        exit 1
+    fi
+
+    download_repos "$1"
+}
+
+main "$@"
