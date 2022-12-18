4f17164055c97e48a5d6a3b1e1cc77d75006d3cf
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-12-18 02:14:04 +0100

    Update recently_modified_files.sh

--- src/recently_modified_files.sh
+++ src/recently_modified_files.sh
@@ -1,3 +1,20 @@
+#!/usr/bin/env bash
+
+# Script Name: recently_modified_files.sh
+# Description: This script lists the most recently modified files in a given directory. 
+# Usage: `recently_modified_files.sh [dir] [n]`
+# Example: `recently_modified_files.sh /home/user/documents 5` lists the 5 most recently modified files in the directory `/home/user/documents`
+
 dir="$1"
 n="$2"
+
+# Set default values for dir and n if no arguments are provided
+if [ -z "$dir" ]; then
+  dir="."
+fi
+
+if [ -z "$n" ]; then
+  n=10
+fi
+
 find "$dir" -type f -printf '%TY-%Tm-%Td %TT %p\n' | sort -r | head -n "$n"
