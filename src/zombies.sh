e7f70aa605c7481af32a5c7f2796f958f3f37931
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-12-18 02:41:39 +0100

    Update zombies.sh

--- src/zombies.sh
+++ src/zombies.sh
@@ -1,1 +1,21 @@
-ps axo pid=,stat= | awk '$2~/^Z/ { print $1 }'
+#!/usr/bin/env bash
+
+# Script Name: zombies.sh
+# Description: This script displays zombie processes, i.e. processes that have terminated but have not been fully removed from the system.
+# Usage: `zombies.sh`
+# Example: `zombies.sh` displays a list of zombie processes.
+
+# Get a list of all processes and their status
+ps -eo pid,stat | sed 1d | awk '{print $1 " " $2}' > /tmp/processes.txt
+
+# Check if each process is a zombie
+while read -r line; do
+  pid=$(echo "$line" | awk '{print $1}')
+  stat=$(echo "$line" | awk '{print $2}')
+  if [[ "$stat" == Z* ]]; then
+    echo "Process $pid is a zombie"
+  fi
+done < /tmp/processes.txt
+
+# Remove the temporary file
+rm /tmp/processes.txt
