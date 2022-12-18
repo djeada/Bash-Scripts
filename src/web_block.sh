3c8beda80869229de91e377737c9650c0117bc55
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-12-18 01:53:57 +0100

    Update web_block.sh

--- src/web_block.sh
+++ src/web_block.sh
@@ -1,56 +1,49 @@
 #!/usr/bin/env bash
 
 # Script Name: web_block.sh
-# Description: Block websites from being visited.
-# Usage: web_block.sh [add|remove] [<website>]
-#      add - Add a website to the block list.
-#      remove - Remove a website from the block list.
-#      <website> - The website to block.
-# Example: web_block.sh add www.example.com
+# Description: A Bash script to block or unblock websites on the local machine by modifying the 'hosts' file.
+# Usage: web_block.sh [-add|-remove] domain
+# Example: web_block.sh -add google.com
 
-append_domain() {
-    local domain="$1"
-    # strip www from url
-    local domain="${domain#www.}"
+# Check if the script is run as root
+if [[ $(id -u) -ne 0 ]]
+then
+    echo "This script must be run as root"
+    exit 1
+fi
 
-    echo "0.0.0.0 $domain" >> /etc/hosts
-    echo "0.0.0.0 www.$domain" >> /etc/hosts
-    echo "::0 $domain" >> /etc/hosts
-    echo "::0 www.$domain" >> /etc/hosts
-}
+# Display usage if no argument is provided
+if [[ $# -lt 2 ]]
+then
+    echo "Usage: $0 [-add|-remove] domain"
+    exit 1
+fi
 
-remove_domain() {
-    local domain="$1"
-    # strip www from url
-    local domain="${domain#www.}"
+# Parse arguments
+operation=$1
+domain=$2
 
+# Strip www from domain
+domain=${domain#www.}
+
+# Check if the operation is valid
+if [[ "$operation" != "-add" && "$operation" != "-remove" ]]
+then
+    echo "Invalid operation: $operation"
+    echo "Usage: $0 [-add|-remove] domain"
+    exit 1
+fi
+
+# Modify the hosts file
+if [[ "$operation" == "-add" ]]
+then
+    # Add the domain to the hosts file
+    echo "127.0.0.1 $domain" >> /etc/hosts
+    echo "127.0.0.1 www.$domain" >> /etc/hosts
+    echo "$domain has been added to the blocklist"
+else
+    # Remove the domain from the hosts file
     sed -i "/$domain/d" /etc/hosts
     sed -i "/www.$domain/d" /etc/hosts
-}
-
-main() {
-
-    if [ $# -ne 2 ]; then
-        echo "Usage: $0 <action> <domain>"
-        echo "  <action>  'add' or 'remove'"
-        echo "  <domain>  Domain to block"
-        exit 1
-    fi
-
-    local action="$1"
-    local domain="$2"
-
-    if [ "$action" == "add" ]; then
-        append_domain "$domain"
-    elif [ "$action" == "remove" ]; then
-        remove_domain "$domain"
-    else
-        echo "Usage: $0 <action> <domain>"
-        echo "  <action>  'add' or 'remove'"
-        echo "  <domain>  Domain to block"
-        exit 1
-    fi
-}
-
-main "$@"
-
+    echo "$domain has been removed from the blocklist"
+fi
