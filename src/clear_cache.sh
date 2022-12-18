3779bad45d170e612136f05147d67807481932d9
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-12-18 02:16:31 +0100

    Update clear_cache.sh

--- src/clear_cache.sh
+++ src/clear_cache.sh
@@ -1,1 +1,15 @@
-find ~/.cache -depth -type f -mtime +7 -delete
+#!/usr/bin/env bash
+
+# Script Name: clear_cache.sh
+# Description: This script clears the local caches in the user's cache directory (e.g. `~/.cache`) that are older than a specified number of days.
+# Usage: `clear_cache.sh [days]`
+# Example: `clear_cache.sh 14` clears the local caches that are older than 14 days in the user's cache directory.
+
+days="$1"
+
+# Set default value for days if no argument is provided
+if [ -z "$days" ]; then
+  days=7
+fi
+
+find ~/.cache -depth -type f -mtime +"$days" -delete
