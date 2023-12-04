8b326461c61241794a17146708e1c307c264b558
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-12-04 21:52:55 +0100

    Update check_if_root.sh

--- src/check_if_root.sh
+++ src/check_if_root.sh
@@ -1,27 +1,58 @@
 #!/usr/bin/env bash
 
 # Script Name: check_if_root.sh
-# Description: This script verifies if it is being run as root and provides a relevant message if not.
-# Usage: ./check_if_root.sh
+# Description: This script verifies if it is being run as root and provides a relevant message if not. Optional logging is included.
+# Usage: ./check_if_root.sh [--log | -l]
+#        Use --log or -l to enable logging.
 
-# Function: Check if the user is root
+LOG_FILE="/var/log/check_if_root.log"
+LOG_ENABLED=0
+
+for arg in "$@"; do
+    case $arg in
+        --log|-l)
+            LOG_ENABLED=1
+            shift # Remove --log or -l from processing
+            ;;
+        *)
+            # Unknown option
+            ;;
+    esac
+done
+
+log_message() {
+    if [ "${LOG_ENABLED}" -eq 1 ]; then
+        echo "$(date +"%Y-%m-%d %T"): $1" >> "${LOG_FILE}"
+    fi
+}
+
 check_root() {
-    # Using id -u to fetch user ID. Root user ID is always 0
-    if [ "$(id -u)" -ne 0 ]; then
+    log_message "Starting root check."
+    USER_ID="$(id -u)"
+    if [ "${USER_ID}" -ne 0 ]; then
         echo "Error: This script must be run as root."
         echo "You may try using: sudo bash $0"
+        log_message "Script attempted without root privileges by user ID ${USER_ID}."
         return 1
     else
         echo "Script is running with root privileges."
+        log_message "Script running as root."
         return 0
     fi
 }
 
-# Function: Main function to control the script flow
 main() {
-    check_root
-    exit $?
+    if check_root; then
+        # Additional code for root actions can go here
+        :
+    fi
+    local exit_status=$?
+    if [ ${exit_status} -ne 0 ]; then
+        log_message "Script exited with error."
+    else
+        log_message "Script completed successfully."
+    fi
+    exit ${exit_status}
 }
 
 main
-
