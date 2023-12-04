bce8d549a84f93b401b13beefe6bd5729748b448
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-12-04 22:05:15 +0100

    Update clear_cache.sh

--- src/clear_cache.sh
+++ src/clear_cache.sh
@@ -1,31 +1,77 @@
 #!/usr/bin/env bash
 
 # Script Name: clear_cache.sh
-# Description: This script clears local caches in the user's cache directory (e.g. `~/.cache`)
-#              that are older than a specified number of days or all caches if '-all' option is provided.
-#              A dry run option '-dry' can also be used to show the files that will be deleted.
-# Usage: `clear_cache.sh [days|-all|-dry]`
-# Example: `clear_cache.sh 14` clears local caches older than 14 days.
-#          `clear_cache.sh -all` clears all local caches.
-#          `clear_cache.sh -dry` shows the files that will be deleted without deleting them.
+# Description: Clears local caches in the specified user's cache directory.
+# Usage: clear_cache.sh [days|-all|-dry] [-d directory] [-l log_file]
+# Example: clear_cache.sh 14 -d ~/.cache - Clears caches older than 14 days in ~/.cache.
 
-days="$1"
+LOG_FILE="/var/log/clear_cache.log"
+LOG_ENABLED=0
+days=0
 dry_run=0
+cache_dir=~/.cache
+
+function log_action {
+    [ $LOG_ENABLED -eq 1 ] && echo "$(date +"%Y-%m-%d %T"): $1" >> $LOG_FILE
+}
+
+function print_usage {
+    echo "Usage: $0 [days|-all|-dry] [-d directory] [-l log_file]"
+    echo "  -d: specify cache directory (default: ~/.cache)"
+    echo "  -l: enable logging to specified log file"
+}
+
+function confirm_deletion {
+    read -p "Are you sure you want to delete these files? [y/N] " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+while getopts ":d:l:" opt; do
+    case $opt in
+        d)
+            cache_dir=$OPTARG
+            ;;
+        l)
+            LOG_FILE=$OPTARG
+            LOG_ENABLED=1
+            ;;
+        \?)
+            print_usage
+            exit 1
+            ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+days=$1
 
 if [ "$days" = "-all" ]; then
     days=0
 elif [ "$days" = "-dry" ]; then
-    days=0
     dry_run=1
+    days=0
 elif ! [[ "$days" =~ ^[0-9]+$ ]] || [[ "$days" -lt 0 ]]; then
-    echo "Please provide a valid number of days or '-all' to clear all caches or '-dry' for a dry run."
+    echo "Invalid argument. Please provide a valid number of days, '-all', or '-dry'."
+    print_usage
     exit 1
 fi
 
 if [[ "$dry_run" -eq 1 ]]; then
-    echo "Dry run. The following files will be deleted:"
-    find ~/.cache -depth -type f -mtime +"$days"
+    echo "Dry run. The following files would be deleted:"
+    find "$cache_dir" -depth -type f -mtime +"$days"
+    log_action "Performed dry run for cache clearing."
 else
-    find ~/.cache -depth -type f -mtime +"$days" -delete
+    echo "Deleting files in $cache_dir older than $days days..."
+    if [ "$days" -eq 0 ] || confirm_deletion; then
+        find "$cache_dir" -depth -type f -mtime +"$days" -delete
+        log_action "Deleted files in cache directory."
+    else
+        echo "Deletion cancelled."
+        log_action "Cache deletion cancelled by user."
+    fi
 fi
-
