3fa7476ca189ab53bd4d01006f7f9fd65e39be42
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-12-04 21:54:44 +0100

    Update web_block.sh

--- src/web_block.sh
+++ src/web_block.sh
@@ -1,62 +1,92 @@
 #!/usr/bin/env bash
 
-# Script Name: web_block
-# Description: This script facilitates blocking or unblocking websites on the local machine by modifying the '/etc/hosts' file.
-# Usage: ./web_block.sh [-add|-remove] domain
-# Example: ./web_block.sh -add google.com
+# Script Name: web_block.sh
+# Description: This script facilitates blocking or unblocking websites by modifying '/etc/hosts'.
+# Usage: sudo ./web_block.sh [-a|-r] domain [-l] [-d]
+# Example: sudo ./web_block.sh -a google.com -l (to block and log)
+
+HOSTS_FILE="/etc/hosts"
+BACKUP_FILE="/etc/hosts.bak"
+LOG_FILE="/var/log/web_block.log"
+LOG_ENABLED=0
+DRY_RUN=0
 
 function print_usage {
-    echo "Usage: $0 [-add|-remove] domain"
+    echo "Usage: $0 [-a|-r] domain [-l] [-d]"
+    echo "  -a: add (block) domain"
+    echo "  -r: remove (unblock) domain"
+    echo "  -l: enable logging"
+    echo "  -d: dry run (no changes made)"
 }
 
-function require_root {
-    # Validate that the script is run as root
-    if [[ $(id -u) -ne 0 ]]
-    then
-        echo "Error: This script must be run as root"
+function log_action {
+    [ $LOG_ENABLED -eq 1 ] && echo "$(date +"%Y-%m-%d %T"): $1" >> $LOG_FILE
+}
+
+function validate_domain {
+    if ! [[ $1 =~ ^([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$ ]]; then
+        echo "Invalid domain name: $1"
         exit 1
     fi
 }
 
-function validate_args {
-    # Validate argument count
-    if [[ $# -lt 2 ]]
-    then
-        echo "Error: Insufficient arguments provided"
-        print_usage
-        exit 1
-    fi
-
-    # Parse and validate arguments
-    operation=$1
-    domain=$2
-    domain=${domain#www.} # Strip 'www' from domain
-
-    if [[ "$operation" != "-add" && "$operation" != "-remove" ]]
-    then
-        echo "Error: Invalid operation: $operation"
-        print_usage
-        exit 1
-    fi
+function backup_hosts {
+    cp $HOSTS_FILE $BACKUP_FILE
 }
 
 function modify_hosts {
-    if [[ "$operation" == "-add" ]]
-    then
-        # Add the domain to the hosts file
-        echo "127.0.0.1 $domain" >> /etc/hosts
-        echo "127.0.0.1 www.$domain" >> /etc/hosts
-        echo "$domain has been blocked successfully."
+    local action_msg
+    if [[ $operation == "add" ]]; then
+        if grep -q "127.0.0.1 $domain" $HOSTS_FILE; then
+            action_msg="$domain is already blocked."
+        else
+            [ $DRY_RUN -eq 0 ] && echo "127.0.0.1 $domain" >> $HOSTS_FILE
+            action_msg="Blocked $domain"
+        fi
     else
-        # Remove the domain from the hosts file
-        sed -i "/^127.0.0.1 $domain$/d" /etc/hosts
-        sed -i "/^127.0.0.1 www.$domain$/d" /etc/hosts
-        echo "$domain has been unblocked successfully."
+        if grep -q "127.0.0.1 $domain" $HOSTS_FILE; then
+            [ $DRY_RUN -eq 0 ] && sed -i "/^127.0.0.1 $domain$/d" $HOSTS_FILE
+            action_msg="Unblocked $domain"
+        else
+            action_msg="$domain is not currently blocked."
+        fi
     fi
+    echo $action_msg
+    log_action $action_msg
 }
 
-# Execute functions
-require_root
-validate_args "$@"
-modify_hosts
+while getopts "arl:d" opt; do
+    case $opt in
+        a)
+            operation="add"
+            ;;
+        r)
+            operation="remove"
+            ;;
+        l)
+            LOG_ENABLED=1
+            ;;
+        d)
+            DRY_RUN=1
+            ;;
+        *)
+            print_usage
+            exit 1
+            ;;
+    esac
+done
 
+shift $((OPTIND -1))
+
+domain=${1#www.}
+
+[ -z "$operation" -o -z "$domain" ] && print_usage && exit 1
+validate_domain $domain
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Error: This script must be run as root"
+    exit 1
+fi
+
+[ $DRY_RUN -eq 0 ] && backup_hosts
+modify_hosts
