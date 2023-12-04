fc2d1818c64ba7b487e89101fd2984edb25006a5
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-12-04 21:56:07 +0100

    Update check_os.sh

--- src/check_os.sh
+++ src/check_os.sh
@@ -1,34 +1,74 @@
 #!/usr/bin/env bash
 
 # Script Name: check_os.sh
-# Description: Identifies the operating system of the current host
-#              and outputs the result to the standard output.
-# Usage: ./check_os.sh
+# Description: Identifies the operating system of the current host and outputs the result.
+# Usage: ./check_os.sh [--json] [--log]
 
-main() {
+LOG_FILE="/var/log/check_os.log"
+JSON_OUTPUT=0
+LOG_ENABLED=0
 
-    os_name=$(uname)
+function log_action {
+    [ $LOG_ENABLED -eq 1 ] && echo "$(date +"%Y-%m-%d %T"): $1" >> $LOG_FILE
+}
+
+function output {
+    if [ $JSON_OUTPUT -eq 1 ]; then
+        echo "{\"platform\":\"$1\", \"distro\":\"$2\", \"version\":\"$3\"}"
+    else
+        echo "Platform: $1"
+        [ -n "$2" ] && echo "Distro: $2"
+        [ -n "$3" ] && echo "Version: $3"
+    fi
+    log_action "Detected OS: Platform=$1, Distro=$2, Version=$3"
+}
+
+function check_os {
+    local os_name=$(uname)
+    local distro=""
+    local version=""
 
     case $os_name in
         Darwin)
-            echo "Mac OS X platform detected"
+            version=$(sw_vers -productVersion)
+            output "Mac OS X" "" "$version"
             ;;
         Linux)
-            distro=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
-            echo "GNU/Linux platform detected"
-            echo "Distro: $distro"
+            if [ -f /etc/os-release ]; then
+                distro=$(awk -F= '/^NAME/{print $2}' /etc/os-release | tr -d '"')
+                version=$(awk -F= '/^VERSION_ID/{print $2}' /etc/os-release | tr -d '"')
+            else
+                distro="Unknown"
+                version="Unknown"
+            fi
+            output "GNU/Linux" "$distro" "$version"
             ;;
         MINGW32_NT*)
-            echo "32 bits Windows NT platform detected"
+            output "Windows NT" "" "32-bit"
             ;;
         MINGW64_NT*)
-            echo "64 bits Windows NT platform detected"
+            output "Windows NT" "" "64-bit"
             ;;
         *)
-            echo "Unsupported platform detected"
+            output "Unsupported" "" ""
             ;;
     esac
 }
 
-main
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --json)
+            JSON_OUTPUT=1
+            ;;
+        --log)
+            LOG_ENABLED=1
+            ;;
+        *)
+            echo "Invalid option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
 
+check_os
