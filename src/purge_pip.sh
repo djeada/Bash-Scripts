a6aca5e1e948d5c59497fdaac14b17d58d0e46d8
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-12-10 16:26:01 +0100

    Update purge_pip.sh

--- src/purge_pip.sh
+++ src/purge_pip.sh
@@ -1,41 +1,38 @@
 #!/bin/bash
 
 # Script Description:
-# This Bash script is designed to purge (uninstall and remove) all user-installed
-# Python packages via pip, while retaining essential packages that are part of
-# the standard Python installation. It identifies non-essential packages installed
-# by the user and removes them, preserving the integrity of essential packages.
+# This script safely uninstalls all user-installed Python packages globally via pip,
+# keeping essential system packages intact.
 
-# List of essential packages
 essential_packages=("pip" "setuptools" "wheel")
+backup_dir="$HOME/python_package_backups"
+backup_file="$backup_dir/backup_$(date +%Y%m%d%H%M%S).txt"
 
-# Function to uninstall and remove a package
 purge_package() {
     local package="$1"
-    pip uninstall -y "$package"
-    pip uninstall -y "$package" # Uninstall twice to ensure removal
-}
-
-# Check if pip is installed
-if ! command -v pip &>/dev/null; then
-    echo "Error: 'pip' is not installed."
-    exit 1
-fi
-
-# List all installed packages via pip, excluding the essential ones
-installed_packages=$(pip freeze | cut -d'=' -f1 | grep -vE "($(IFS="|"; echo "${essential_packages[*]}"))")
-
-# Purge non-essential packages
-for package in $installed_packages; do
     echo "Purging non-essential package: $package"
-    purge_package "$package"
-
-    # Check the uninstallation status
+    pip uninstall -y "$package"
     if [[ $? -eq 0 ]]; then
         echo "Successfully purged: $package"
     else
         echo "Error: Failed to purge $package"
     fi
+}
+
+if ! command -v pip &>/dev/null; then
+    echo "Error: 'pip' is not installed globally."
+    exit 1
+fi
+
+mkdir -p "$backup_dir"
+
+installed_packages=$(pip freeze | cut -d'=' -f1 | grep -vE "($(IFS="|"; echo "${essential_packages[*]}"))")
+
+echo "$installed_packages" > "$backup_file"
+echo "Backup of installed packages created at $backup_file"
+
+for package in $installed_packages; do
+    purge_package "$package"
 done
 
-echo "Package purge completed."
+echo "Global package purge completed."
