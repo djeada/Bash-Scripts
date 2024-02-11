b3f6e1c11f0ffa7ecd8f5fb8a84353dd03daba87
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2024-02-11 20:21:36 +0100

    Update cpu_temp.sh

--- src/cpu_temp.sh
+++ src/cpu_temp.sh
@@ -8,36 +8,42 @@
 # Function to get the CPU temperature on Linux
 get_linux_cpu_temp() {
     local temp_paths=(
-        "/sys/class/thermal/thermal_zone0/temp"       # Common thermal zone
-        "/sys/class/hwmon/hwmon*/temp1_input"         # HW Monitor
-        "/sys/devices/virtual/thermal/thermal_zone*/temp" # Virtual devices
+        "/sys/class/thermal/thermal_zone0/temp"
+        "/sys/class/hwmon/hwmon*/temp1_input"
+        "/sys/devices/virtual/thermal/thermal_zone*/temp"
     )
 
-    local path temp
-    for potential in "${temp_paths[@]}"; do
-        # Use the first accessible and readable file
-        for file in "$potential"/*; do
+    for path in "${temp_paths[@]}"; do
+        for file in $path; do
             if [ -r "$file" ]; then
-                path="$file"
-                break 2
+                local temp=$(cat "$file")
+                temp=$((temp/1000))
+                echo "CPU temp: $temp°C"
+                return
             fi
         done
     done
 
-    if [ -z "$path" ]; then
-        echo "Could not find a valid temperature file in common paths."
-        exit 1
-    fi
-
-    temp=$(cat "$path")
-    temp=$((temp/1000))
-
-    echo "CPU temp: $temp C"
+    echo "Could not find a valid temperature file in common paths."
+    exit 1
 }
 
 # Function to get the CPU temperature on MacOS
 get_macos_cpu_temp() {
-    osascript -e 'tell app "System Events" to tell process "SystemUIServer" to tell (menu bar item 2 of menu bar 1 where description is "systemUIServer") to value' | cut -d " " -f6 | sed 's/.$//g'
+    if ! command -v osascript &> /dev/null; then
+        echo "Error: Required command 'osascript' not found" >&2
+        exit 1
+    fi
+
+    local temp
+    temp=$(osascript -e 'tell application "System Events" to get CPU temperature of sensor 1' || echo "Error")
+    
+    if [[ $temp == "Error" ]]; then
+        echo "Could not retrieve CPU temperature. Ensure you have the necessary permissions."
+        exit 1
+    fi
+
+    echo "CPU temp: $temp°C"
 }
 
 main() {
@@ -49,4 +55,3 @@ main() {
 }
 
 main "$@"
-
