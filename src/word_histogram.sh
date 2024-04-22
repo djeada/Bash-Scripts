9ed5b37885f69c934ac2ffdca066e388b39d191c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2024-04-22 21:36:02 +0200

    Update word_histogram.sh

--- src/word_histogram.sh
+++ src/word_histogram.sh
@@ -6,43 +6,63 @@ remove_diacritics()
     sed -i 'y/ąāáǎàćēéěèęīíǐìłńōóǒòóśūúǔùǖǘǚǜżźĄĀÁǍÀĆĒĘÉĚÈĪÍǏÌŁŃŌÓǑÒÓŚŪÚǓÙǕǗǙǛŻŹ/aaaaaceeeeeiiiilnooooosuuuuuuuuzzAAAAACEEEEEIIIILNOOOOOSUUUUUUUUZZ/' "$1"
 }
 
-# Check if file is given as argument
-if [ $# -lt 1 ] || [ $# -gt 2 ]; then
-    echo "Usage: $0 filename [min_word_length]"
-    exit 1
-fi
+# Process text to calculate word frequencies
+process_text() {
+    local min_word_length=$2
+    declare -A wordcounts
 
-# Check if file exists
-if [ ! -f "$1" ]; then
-    echo "File $1 not found."
-    exit 1
-fi
+    while read -r line; do
+        line=$(remove_diacritics "$line")
+        line=$(echo "$line" | tr -dc '[:alpha:][:space:]')
 
-# If second argument is present, set min_word_length
+        for word in $line; do
+            word=${word,,}  # Convert to lowercase
+            if [ ${#word} -ge "$min_word_length" ]; then
+                ((wordcounts[$word]++))
+            fi
+        done
+    done
+
+    # Print word frequencies
+    for word in "${!wordcounts[@]}"; do
+        echo "$word:${wordcounts[$word]}"
+    done | sort -rn -t":" -k2
+}
+
+# Main program starts here
 min_word_length=0
-if [ -n "$2" ]; then
-    min_word_length=$2
-fi
+file_mode=false
 
-# Calculate word frequencies
-echo "Calculating word frequencies..."
-declare -A wordcounts
-while read -r line
-do
-    line=$(remove_diacritics "$line") # remove diacritics
-    line=$(echo "$line" | tr -dc '[:alpha:][:space:]') # remove non-letter characters
-    for word in $line
-    do
-        word=${word,,} # convert to lowercase
-        if [ ${#word} -ge "$min_word_length" ]; then
-            ((wordcounts[$word]++))
+while getopts ":l:" opt; do
+    case $opt in
+        l)
+            min_word_length=$OPTARG
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+# Check input method
+if [ "$#" -eq 0 ]; then
+    # Reading from standard input (pipe or redirection)
+    process_text "/dev/stdin" "$min_word_length"
+else
+    # Reading from files
+    for filename in "$@"; do
+        if [ -f "$filename" ]; then
+            echo "Processing $filename..."
+            process_text "$filename" "$min_word_length"
+        else
+            echo "File $filename not found."
+            exit 1
         fi
     done
-done < "$1"
-
-# Sort and print word frequencies
-echo "Printing sorted word frequencies..."
-for word in "${!wordcounts[@]}"; do
-    echo "$word:${wordcounts[$word]}"
-done | sort -rn -t":" -k2
-
+fi
