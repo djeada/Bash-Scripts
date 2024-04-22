c3a93bbf70de0de5376eb9f2687d87311c26d1ec
djeada <addjellouli1@gmail.com>
2024-04-22 21:55:34 +0200

    [bug-fix]: correct parallel processing on word histogram

--- src/word_histogram.sh
+++ src/word_histogram.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Function to remove diacritics
+# Function to remove diacritics from a line of text
 remove_diacritics()
 {
-    sed -i 'y/ąāáǎàćēéěèęīíǐìłńōóǒòóśūúǔùǖǘǚǜżźĄĀÁǍÀĆĒĘÉĚÈĪÍǏÌŁŃŌÓǑÒÓŚŪÚǓÙǕǗǙǛŻŹ/aaaaaceeeeeiiiilnooooosuuuuuuuuzzAAAAACEEEEEIIIILNOOOOOSUUUUUUUUZZ/' "$1"
+    echo "$1" | sed 'y/ąāáǎàćēéěèęīíǐìłńōóǒòóśūúǔùǖǘǚǜżźĄĀÁǍÀĆĒĘÉĚÈĪÍǏÌŁŃŌÓǑÒÓŚŪÚǓÙǕǗǙǛŻŹ/aaaaaceeeeeiiiilnooooosuuuuuuuuzzAAAAACEEEEEIIIILNOOOOOSUUUUUUUUZZ/'
 }
 
 # Process text to calculate word frequencies
@@ -12,7 +12,7 @@ process_text() {
     local min_word_length=$2
     declare -A wordcounts
 
-    while read -r line; do
+    while IFS= read -r line; do
         line=$(remove_diacritics "$line")
         line=$(echo "$line" | tr -dc '[:alpha:][:space:]')
 
@@ -56,20 +56,23 @@ while getopts ":l:j" opt; do
 done
 shift $((OPTIND -1))
 
+# Correct the usage of parallel to ensure file names are treated correctly and outputs are merged
 if [ "$#" -eq 0 ]; then
     # No files provided, reading from stdin
+    input="/dev/stdin"
     if $output_json; then
-        process_text "/dev/stdin" "$min_word_length" | jq -Rn '[inputs | split(":") | {(.[0]): .[1]|tonumber}] | add'
+        process_text "$input" "$min_word_length" | jq -Rn '[inputs | split(":") | {(.[0]): .[1]|tonumber}] | add'
     else
-        process_text "/dev/stdin" "$min_word_length"
+        process_text "$input" "$min_word_length" | sort -t: -k2,2nr
     fi
 else
-    # Process files in parallel
+    # Process files in parallel and merge results
+    export min_word_length
+    export output_json
     if $output_json; then
-        parallel --will-cite "process_text {} $min_word_length | jq -Rn '[inputs | split(\":\") | {(.[0]): .[1]|tonumber}] | add'" ::: "$@" | jq -s 'add'
+        parallel --will-cite 'process_text {} $min_word_length | jq -Rsn "[inputs | split(\":\") | {(.[0]): .[1]|tonumber}]" | add' ::: "$@" | jq -s 'add | to_entries | sort_by(.value) | .[] | "\(.[key]):\(.[value])"' | jq -s 'from_entries'
     else
-        parallel --will-cite process_text {} $min_word_length ::: "$@"
+        parallel --will-cite 'process_text {} $min_word_length' ::: "$@" | sort -t: -k2,2nr
     fi
 fi
-
 
