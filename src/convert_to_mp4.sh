31bb33d593220d3fdabd32478f2fc52ad691a9a1
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2024-05-29 20:06:10 +0200

    Update convert_to_mp4.sh

--- src/convert_to_mp4.sh
+++ src/convert_to_mp4.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Script Name: convert_to_mp4.sh
+# Description: Converts a given video file to MP4 format.
+# Usage: ./convert_to_mp4.sh <file_path>
+# Example: ./convert_to_mp4.sh video.avi
+
+LOG_FILE="/var/log/convert_to_mp4.log"
+LOG_ENABLED=1
+
+log_action() {
+    [ $LOG_ENABLED -eq 1 ] && echo "$(date +"%Y-%m-%d %T"): $1" >> $LOG_FILE
+}
 
 # Function to check if a command exists
 command_exists() {
@@ -8,39 +20,51 @@ command_exists() {
 # Check for required dependencies
 if ! command_exists ffmpeg; then
     echo "Error: ffmpeg is not installed. Please install it and try again."
+    log_action "Error: ffmpeg is not installed."
     exit 1
 fi
 
 # Usage function
 usage() {
     echo "Usage: $0 <file_path>"
+    log_action "Displayed usage information."
 }
 
-# Check if an argument was provided
-if [ "$#" -ne 1 ]; then
-    usage
-    exit 1
-fi
+# Function to process the video file
+convert_to_mp4() {
+    local file_path=$1
+    local basename=$(basename "$file_path")
+    local filename="${basename%.*}"
+    local output_file="${filename}.mp4"
 
-FILE_PATH=$1
+    log_action "Starting conversion of $file_path to $output_file."
+    if ffmpeg -i "$file_path" -vcodec libx264 -acodec aac "$output_file"; then
+        echo "Successfully converted $file_path to $output_file"
+        log_action "Successfully converted $file_path to $output_file."
+    else
+        echo "Error: Conversion failed."
+        log_action "Error: Conversion failed for $file_path."
+        exit 1
+    fi
+}
 
-# Check if the file exists
-if [ ! -f "$FILE_PATH" ]; then
-    echo "Error: File does not exist."
-    exit 1
-fi
+main() {
+    if [ "$#" -ne 1 ]; then
+        usage
+        log_action "Error: No file path provided."
+        exit 1
+    fi
 
-# Get the file name without extension
-BASENAME=$(basename "$FILE_PATH")
-FILENAME="${BASENAME%.*}"
+    local file_path=$1
 
-# Set the output file name
-OUTPUT_FILE="${FILENAME}.mp4"
+    # Check if the file exists
+    if [ ! -f "$file_path" ]; then
+        echo "Error: File does not exist."
+        log_action "Error: File does not exist at path $file_path."
+        exit 1
+    fi
 
-# Convert the file
-if ffmpeg -i "$FILE_PATH" -vcodec libx264 -acodec aac "$OUTPUT_FILE"; then
-    echo "Successfully converted $FILE_PATH to $OUTPUT_FILE"
-else
-    echo "Error: Conversion failed."
-    exit 1
-fi
+    convert_to_mp4 "$file_path"
+}
+
+main "$@"
