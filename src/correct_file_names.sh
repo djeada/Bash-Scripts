aa305abb92dfc58abc43e04fde0616092e13c846
djeada <addjellouli1@gmail.com>
2024-08-25 22:51:29 +0200

    [feature]: improve correct_file_names.sh

--- src/correct_file_names.sh
+++ src/correct_file_names.sh
@@ -3,16 +3,24 @@
 # Script Name: correct_file_names.sh
 # Description: This script corrects file names in a specified directory by replacing all non-alphanumeric characters
 #              (excluding dots) with underscores, converting repeated underscores to a single underscore, and making the names lowercase.
-#              Additionally, it provides an option to include hidden directories and an option to exclude certain files from being changed.
-# Usage: correct_file_names.sh [-a] [-e file1,file2,...] <directory>
+#              Additionally, it provides an option to include hidden directories.
+# Usage: correct_file_names.sh [-a] [-e <file1,file2,...>] <directory>
 #        -a: Include hidden directories (default is false).
-#        -e: Comma-separated list of file names to exclude from changes (default is "README.md").
+#        -e: Comma-separated list of file names to exclude from modification (default is 'README.md').
 #        <directory>: The directory containing the files to be corrected.
-# Example: ./correct_file_names.sh -a -e README.md,example.txt path/to/directory
+# Example: ./correct_file_names.sh -a -e README.md,path/to/file.txt path/to/directory
 
 correct_file_name() {
     local old_name="$1"
     local new_name
+
+    # Extract filename from the path
+    local filename=$(basename "$old_name")
+
+    # Check if the filename is in the excluded list
+    if [[ " ${excluded_files[@]} " =~ " ${filename} " ]]; then
+        return
+    fi
 
     # Replace all punctuation characters with underscores
     new_name=$(echo "$old_name" | sed -e 's/[^a-zA-Z0-9._\/]/_/g')
@@ -37,27 +45,23 @@ correct_file_name() {
 find_files() {
     local dir="$1"
     local include_hidden="$2"
-    local exclusions="$3"
-    local find_options="-maxdepth 1"
-    
+    local find_options=(-maxdepth 1)
+
     if [ "$include_hidden" != true ]; then
-        find_options="$find_options ! -path '*/.*'"
+        find_options+=(! -regex '.*/\..*')
     fi
 
-    find "$dir" $find_options -print0 | while IFS= read -r -d $'\0' file; do
-        local base_file=$(basename "$file")
-        if [[ ! "$exclusions" =~ (^|,)$base_file(,|$) ]]; then
-            correct_file_name "$file"
-        fi
+    find "$dir" "${find_options[@]}" -print0 | while IFS= read -r -d $'\0' file; do
+        correct_file_name "$file"
         if [ "$dir" != "$file" ] && [ -d "$file" ]; then
-            find_files "$file" "$include_hidden" "$exclusions"
+            find_files "$file" "$include_hidden"
         fi
     done
 }
 
 main() {
     local include_hidden=false
-    local exclusions="README.md"
+    local excluded_files=("README.md")  # Default excluded file
 
     while getopts ":ae:" opt; do
         case $opt in
@@ -65,7 +69,7 @@ main() {
                 include_hidden=true
                 ;;
             e)
-                exclusions="$exclusions,$OPTARG"
+                IFS=',' read -r -a excluded_files <<< "$OPTARG"
                 ;;
             \?)
                 echo "Invalid option: -$OPTARG" >&2
@@ -76,22 +80,16 @@ main() {
     shift $((OPTIND - 1))
 
     if [ $# -eq 0 ]; then
-        echo "Usage: $0 [-a] [-e file1,file2,...] <directory>"
+        echo "Usage: $0 [-a] [-e <file1,file2,...>] <directory>"
         exit 1
     fi
 
     local dir="$1"
 
-    # Convert the provided directory to an absolute path
-    dir=$(realpath "$dir")
-
-    if [ -d "$dir" ]; then
-        find_files "$dir" "$include_hidden" "$exclusions"
+    if [ "$dir" == '.' ] || [ -d "$dir" ]; then
+        find_files "$dir" "$include_hidden"
     elif [ -f "$dir" ]; then
-        local base_file=$(basename "$dir")
-        if [[ ! "$exclusions" =~ (^|,)$base_file(,|$) ]]; then
-            correct_file_name "$dir"
-        fi
+        correct_file_name "$dir"
     else
         echo "$dir is not a valid path!"
         exit 1
@@ -99,3 +97,4 @@ main() {
 }
 
 main "$@"
+
